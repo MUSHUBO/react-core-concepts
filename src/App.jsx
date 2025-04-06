18d3e464ad9759a29ab921580c296d6a43b71983
MUSHUBO <shubo3132@gmail.com>
2025-04-07 04:15:44 +0600

    friends list for give me treat

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,40 @@ import './App.css'
 import Counter from './Counter';
 import Batsman from './Batsman';
 import Users from './Users';
+import { Suspense } from 'react';
+import Friends from './Friends';
+
+
+const fetchUsers = fetch(`https://jsonplaceholder.typicode.com/users`)
+  .then(res => res.json())
+
+const fetchFriends = async() => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users`)
+  return res.json();
+};
+
+
 
 function App() {
-  
+
+  const friendsPromise = fetchFriends();
+
   return (
     <>
       <h1>Vite + React</h1>
 
-      <Users></Users>
+      <Suspense fallback={<h3>Waiting...</h3>}>
+        <Friends friendsPromise={friendsPromise}></Friends>
+      </Suspense>
+
+      <Suspense fallback={<h3>Loading...</h3>}>
+        <Users fetchUsers={fetchUsers}></Users>
+      </Suspense>
+
       <Batsman></Batsman>
       <Counter></Counter>
 
-      
+
     </>
   )
 }
--- src/Summery.jsx
+++ src/Summery.jsx
@@ -13,6 +13,12 @@
  * 
  */
 
+/**
+ * 1. just write a simple fetch with json conversion.
+ * 2. wrap the data loading component under suspense.
+ * 
+ */
+
 
 // fetch(`https://jsonplaceholder.typicode.com/users`)
 //     .then(res => res.json())
